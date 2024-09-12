4acbd00b9364997918368354e03fedc0ec508332
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2024-09-12 14:11:26 -0300

    add time to clean

--- src/broom_handle.gd
+++ src/broom_handle.gd
@@ -1,15 +1,20 @@
 extends MeshInstance3D
 
-signal cleaned
+var cleanees:Array[Area3D] = []
 
+@export var cleaning_rate : float = 250.0
 @onready var wave_animation: AnimationPlayer = $WaveAnimation
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if Input.is_action_just_pressed('wave'):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		wave_animation.play('wave')
-
+	for area in cleanees:
+		area.call('clean', delta * cleaning_rate)
 
 func _on_sweep_area_3d_area_entered(area: Area3D) -> void:
-	cleaned.emit()
-	area.queue_free()
+	cleanees.append(area)
+
+func _on_sweep_area_3d_area_exited(area: Area3D) -> void:
+	if area in cleanees:
+		cleanees.pop_at(cleanees.find(area))
--- src/stain_area.gd
+++ src/stain_area.gd
@@ -1,5 +1,15 @@
 extends Area3D
 @onready var stain_square: MeshInstance3D = $StainSquare
 
+var clean_amount : float = 100.0
+
 func _ready() -> void:
 	stain_square.rotation_degrees.z = randf_range(0,360)
+
+func clean(amount: float) -> void:
+	clean_amount -= amount
+	if clean_amount <= 0:
+		queue_free()
+	else:
+		stain_square.transparency = clean_amount / 100.0
+	print(clean_amount / 100.0)
